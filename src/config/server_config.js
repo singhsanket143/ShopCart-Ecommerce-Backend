da1978665b9a2fc5234165be891b00303dc5da8c
Sanket Singh <singhsanket143@gmail.com>
2023-12-15 21:28:01 +0530

    Manually connected mysql db with express

--- src/config/server_config.js
+++ src/config/server_config.js
@@ -3,5 +3,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL,
+    DB_USER: process.env.DB_USER,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASS: process.env.DB_PASS,
+
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const responseTime = require('response-time');
+const mysql = require('mysql2');
 
+const { PORT, DB_NAME, DB_PASS, DB_URL, DB_USER } = require('./config/server_config');
 
-const { PORT } = require('./config/server_config');
+const db = mysql.createConnection({
+    host: DB_URL,
+    user: DB_USER,
+    password: DB_PASS,
+    database: DB_NAME
+})
+    
 
 const ApiRouter = require('./routes/api_router');
 
@@ -24,4 +32,23 @@ app.use('/api', ApiRouter); // if any req comes with url starting with /api
 
 app.listen(PORT, () => {
     console.log(`Server for Shopcart is Up ${PORT}`);
+
+   db.connect((err) => {
+        if(err) {
+            console.log("DB didnt connect");
+            console.log(err);
+            throw err;
+        }
+        console.log('DB Connected');
+        db.query('Select * from products', (err, result) => {
+            if(err) {
+                console.log(err);
+                throw err;
+            }
+            console.log(result[0]);
+        })
+   });
+
+
+
 })
