2a8aa8c05df72eec64150623688fc445c6fe7d49
Sanket Singh <singhsanket143@gmail.com>
2023-12-27 22:13:16 +0530

    Added token verification method

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -5,6 +5,10 @@ function generateJWT(payload) {
     return jwt.sign(payload, JWT_SECRET, {expiresIn: JWT_EXPIRY})
 }
 
+function verifyToken(token) {
+    return jwt.verify(token, JWT_SECRET)
+}
+
 module.exports = {
     generateJWT
 }
